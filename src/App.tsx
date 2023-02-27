e01c752eec3152fb16376f0683baa4308e324cd3
Ashish Baravaliya <ashishbaravaliya@Ashishs-Air.cgocable.net>
2023-02-27 18:58:56 -0500

    feat: hero section  added

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="app">
       <Navbar />
-      {/* <Header /> */}
+      <Header />
       {/* <About /> */}
       {/* <Work /> */}
       {/* <Skills /> */}
--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -1,7 +1,57 @@
-import "./Header.scss";
+import { motion } from "framer-motion";
 
-const Header: React.FC = () => {
-  return <div>Header</div>;
+// import { AppWrap } from "../../wrapper";
+// import { images } from "../../constants";
+import "./Header.scss";
+import React from "react";
+
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: "easeInOut",
+    },
+  },
 };
 
+const Header: React.FC = () => (
+  <div className="app__header app__flex">
+    <motion.div
+      whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+      transition={{ duration: 0.5 }}
+      className="app__header-info"
+    >
+      <div className="app__header-badge">
+        <div className="badge-cmp app__flex">
+          <span>👋</span>
+          <div style={{ marginLeft: 20 }}>
+            <p className="p-text">Hello, I am</p>
+            <h1 className="head-text">Ashish</h1>
+          </div>
+        </div>
+
+        <div className="tag-cmp app__flex">
+          <p className="p-text">Full Stack Developer</p>
+          <p className="p-text">Freelancer</p>
+        </div>
+      </div>
+    </motion.div>
+
+    {/* <motion.div
+      variants={scaleVariants}
+      whileInView={scaleVariants.whileInView}
+      className="app__header-circles"
+    >
+      {[images.flutter, images.redux, images.sass].map((circle, index) => (
+        <div className="circle-cmp app__flex" key={`circle-${index}`}>
+          <img src={circle} alt="profile_bg" />
+        </div>
+      ))}
+    </motion.div> */}
+  </div>
+);
+
 export default Header;
+// export default AppWrap(Header, "home");
