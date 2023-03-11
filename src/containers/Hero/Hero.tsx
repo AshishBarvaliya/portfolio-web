2788dd5651d40c9877bf7ca506a721a264096102
Ashish Baravaliya <ashishbaravaliya@Ashishs-Air.cgocable.net>
2023-03-10 19:25:28 -0500

    update: buttons added to hero section

--- src/containers/Hero/Hero.tsx
+++ src/containers/Hero/Hero.tsx
@@ -1,57 +1,37 @@
 import { motion } from "framer-motion";
 
-// import { AppWrap } from "../../wrapper";
-// import { images } from "../../constants";
 import "./Hero.scss";
 import React from "react";
+import Button from "../../components/Button/Button";
 
-const scaleVariants = {
-  whileInView: {
-    scale: [0, 1],
-    opacity: [0, 1],
-    transition: {
-      duration: 1,
-      ease: "easeInOut",
-    },
-  },
-};
+const email = process.env.REACT_APP_CONTACT_EMAIL;
 
 const Hero: React.FC = () => (
   <div className="app__hero app__flex">
     <motion.div
-      whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+      whileInView={{ y: [100, 0], opacity: [0, 1] }}
       transition={{ duration: 0.5 }}
       className="app__hero-info"
     >
-      <div className="app__hero-badge">
-        <div className="badge-cmp app__flex">
-          <span>👋</span>
-          <div style={{ marginLeft: 20 }}>
-            <p className="p-text">Hello, I am</p>
-            <h1 className="head-text">Ashish</h1>
+      <div className="app__hero-content">
+        <div className="app__flex">
+          <div>
+            <p className="p-text">Hello, My name is</p>
+            <h1 className="head-text">Ashish Baravaliya</h1>
           </div>
         </div>
 
-        <div className="tag-cmp app__flex">
+        <div className="app__flex">
           <p className="p-text">Full Stack Developer</p>
           <p className="p-text">Freelancer</p>
         </div>
       </div>
+      <div className="app__hero-button-group">
+        <Button label={"Contact"} link={`mailto:${email}`} />
+        <Button label={"Resume"} />
+      </div>
     </motion.div>
-
-    {/* <motion.div
-      variants={scaleVariants}
-      whileInView={scaleVariants.whileInView}
-      className="app__hero-circles"
-    >
-      {[images.flutter, images.redux, images.sass].map((circle, index) => (
-        <div className="circle-cmp app__flex" key={`circle-${index}`}>
-          <img src={circle} alt="profile_bg" />
-        </div>
-      ))}
-    </motion.div> */}
   </div>
 );
 
 export default Hero;
-// export default AppWrap(Hero, "home");
