d15c3b95ea97873366d626d6bdb71f3b295fff0f
Ashish Baravaliya <ashishbaravaliya@Ashishs-Air.cgocable.net>
2023-03-10 19:21:00 -0500

    update: footer design updated

--- src/containers/Footer/Footer.tsx
+++ src/containers/Footer/Footer.tsx
@@ -1,104 +1,39 @@
-import React, { useState } from "react";
+import { motion } from "framer-motion";
+import React from "react";
+import { Credits } from "../../components";
+import Button from "../../components/Button/Button";
 
-import { images } from "../../constants";
-import { client } from "../../sanityClient";
 import "./Footer.scss";
 
 const Footer: React.FC = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-  const { name, email, message } = formData;
-
-  const handleChangeInput = (e: any) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleSubmit = () => {
-    setLoading(true);
-
-    const contact = {
-      _type: "contact",
-      name: formData.name,
-      email: formData.email,
-      message: formData.message,
-    };
-
-    client
-      .create(contact)
-      .then(() => {
-        setLoading(false);
-        setIsFormSubmitted(true);
-      })
-      .catch((err) => console.log(err));
-  };
+  const email = process.env.REACT_APP_CONTACT_EMAIL;
 
   return (
     <>
-      <h2 id="contact" className="head-text">
-        Take a coffee & chat with me
-      </h2>
-
-      <div className="app__footer-cards">
-        <div className="app__footer-card ">
-          <img src={images.email} alt="email" />
-          <a href="mailto:hello@micael.com" className="p-text">
-            hello@micael.com
-          </a>
-        </div>
-        <div className="app__footer-card">
-          <img src={images.mobile} alt="phone" />
-          <a href="tel:+1 (123) 456-7890" className="p-text">
-            +1 (123) 456-7890
-          </a>
-        </div>
+      <div className="app__footer">
+        <motion.div
+          whileInView={{ y: [100, 0], opacity: [0, 1] }}
+          transition={{ duration: 0.5 }}
+          className="app__footer-info"
+        >
+          <h2 id="contact" className="head-text">
+            Get In Touch
+          </h2>
+          <div className="app_footer-content">
+            <text>
+              If you're in search of an outstanding full-stack developer with
+              remarkable front-end expertise, an unyielding problem-solving
+              mindset, and a collaborative spirit, then I am your ideal
+              candidate. You can count on me to be available at all times.
+            </text>
+            <div className="app_footer-button-wrapper">
+              <Button label="Say Hello" link={`mailto:${email}`} width="lg" />
+            </div>
+          </div>
+        </motion.div>
       </div>
-      {!isFormSubmitted ? (
-        <div className="app__footer-form app__flex">
-          <div className="app__flex">
-            <input
-              className="p-text"
-              type="text"
-              placeholder="Your Name"
-              name="username"
-              value={name}
-              onChange={handleChangeInput}
-            />
-          </div>
-          <div className="app__flex">
-            <input
-              className="p-text"
-              type="email"
-              placeholder="Your Email"
-              name="email"
-              value={email}
-              onChange={handleChangeInput}
-            />
-          </div>
-          <div>
-            <textarea
-              className="p-text"
-              placeholder="Your Message"
-              value={message}
-              name="message"
-              onChange={handleChangeInput}
-            />
-          </div>
-          <button type="button" className="p-text" onClick={handleSubmit}>
-            {!loading ? "Send Message" : "Sending..."}
-          </button>
-        </div>
-      ) : (
-        <div>
-          <h3 className="head-text">Thank you for getting in touch!</h3>
-        </div>
-      )}
+
+      <Credits />
     </>
   );
 };
