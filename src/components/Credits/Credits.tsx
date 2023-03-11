65a7f42583c8f9c1ca522fa283788be4768f32d5
Ashish Baravaliya <ashishbaravaliya@Ashishs-Air.cgocable.net>
2023-03-10 19:23:50 -0500

    feat: credit component added

---
+++ src/components/Credits/Credits.tsx
@@ -0,0 +1,13 @@
+import "./Credits.scss";
+
+const Credits: React.FC = () => {
+  return (
+    <div className="app__credits-div">
+      <a href="https://github.com/AshishBarvaliya">
+        Desinged & Built by Ashish Baravaliya
+      </a>
+    </div>
+  );
+};
+
+export default Credits;
