6e18a35b819f0292f00ab22e58064e8e3bc1fa2c
Ashish Baravaliya <ashishbaravaliya@Ashishs-Air.cgocable.net>
2023-03-16 09:34:16 -0400

    fix: ts fix

--- src/hooks/useAnalyticsEventTracker.tsx
+++ src/hooks/useAnalyticsEventTracker.tsx
@@ -1,6 +1,8 @@
 import ReactGA from "react-ga";
 
-const useAnalyticsEventTracker = (category = "Unknown category") => {
+const useAnalyticsEventTracker = (
+  category = "Unknown category"
+): ((action?: string, label?: string) => void) => {
   const eventTracker = (action = "test action", label = "test label") => {
     ReactGA.event({ category, action, label });
   };
